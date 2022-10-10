6696b2c7486bf9c43693dee8af46b0a81f1ea81e
hagverdi11 <hagverdiam@code.edu.az>
2022-10-11 02:11:25 +0400

    Added task

---
+++ EntityFrameworkProject/Areas/AdminArea/Controllers/SliderDetailsController.cs
@@ -0,0 +1,27 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.Areas.AdminArea.Controllers
+{
+    public class SliderDetailsController: Controller
+    {
+        private readonly AppDbContext _context;
+
+        public SliderDetailsController (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            IEnumerable<SliderDetail> sliderDetails = await _context.SliderDetails.ToListAsync();
+            return View(sliderDetails);
+        }
+    }
+}
